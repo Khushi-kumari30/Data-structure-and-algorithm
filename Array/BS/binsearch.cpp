4e5854a60d4a33634c799819c4e8cc0c666bc56d
Khushi Kumari <khushi@example.com>
2026-02-26 21:39:57 +0530

    Binary Search

---
+++ Array/BS/binsearch.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>a = {1,2,2,2,3,4};
+    int target = 2;
+    int left = 0;
+    int n = a.size();
+    int right = n-1;
+    int mid;
+    while(left <= right){
+        mid = left + (right - left)/2;
+        if(a[mid] == target){
+            cout<<mid;
+            break;
+        }
+        else
+        if(a[mid] < target){
+            left = mid + 1;
+        }else{
+            right = mid -1;
+        }
+        //cout<<mid;
+    }
+   // cout <<mid;
+}
