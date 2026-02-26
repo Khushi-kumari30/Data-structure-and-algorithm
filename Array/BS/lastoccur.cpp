c068d083c1ebd83ded930121006cbbb149f4ff4b
Khushi Kumari <khushi@example.com>
2026-02-26 22:00:26 +0530

    Add last occurrence logic

---
+++ Array/BS/lastoccur.cpp
@@ -0,0 +1,28 @@
+//Last Occurrence of Target
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>a = {1,2,2,2,3,4};
+    int target = 2;
+    int left = 0;
+    int n = a.size();
+    int right = n-1;
+    int mid;
+    int ans = 0;
+    while(left <= right){
+        mid = left + (right - left)/2;
+        if(a[mid] == target){
+        ans = mid;
+        left = mid+1;
+        }
+        else
+        if(a[mid] < target){
+            left = mid + 1;
+        }else{
+            right = mid -1;
+        }
+        //cout<<mid;
+    }
+   cout <<ans;
+}
